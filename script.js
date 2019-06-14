e3f309a85d8e89384bfccbdaeae363bab2738094
MrBLightning <44550952+MrBLightning@users.noreply.github.com>
2019-06-14 16:23:18 +0300

    some small changes to script.js
    copied over code from names.addEventListener("keyup", function(event) into document.getElementById("add").onclick 
    to make the add button add names rather than just display the last name entered.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 console.log("script.js connected");
 
-var inputs = document.getElementById("inputs");
-var ul = document.getElementById("list");
-var addNameBtn = document.getElementById("add");
-var clearListBtn = document.getElementById("clear");
-var pickBtn = document.getElementById("pick");
-var textBox = document.getElementById("names");
-var pickedDisplay = document.getElementById('pickedDisplay');
-var deleteThis = document.getElementsByClassName('deleteThis');
-var li = document.createElement('li');
-var $ = function (selector) {
+let inputs = document.getElementById("inputs");
+let ul = document.getElementById("list");
+let addNameBtn = document.getElementById("add");
+let clearListBtn = document.getElementById("clear");
+let pickBtn = document.getElementById("pick");
+let textBox = document.getElementById("names");
+let pickedDisplay = document.getElementById('pickedDisplay');
+let deleteThis = document.getElementsByClassName('deleteThis');
+// let li = document.createElement('li');
+let $ = function (selector) {
     return document.querySelector(selector);
 };
 
@@ -18,22 +18,22 @@ var $ = function (selector) {
 names.addEventListener("keyup", function(event){
 
         if (event.keyCode === 13) {
-            var names = document.getElementById("names").value;
-            var li = document.createElement('li');
-                li.contentEditable = 'true';
-            var span = document.createElement('span');
-                span.innerHTML = 'x';
-                span.className = 'deleteThis';
-            var sDel = '<span class="deleteThis">x</span>';
+            let names = document.getElementById("names").value;
+            let li = document.createElement('li');
+            li.contentEditable = 'true';
+            let span = document.createElement('span');
+            span.innerHTML = 'x';
+            span.className = 'deleteThis';
+            let sDel = '<span class="deleteThis">x</span>';
             if (textBox.value!==''){
 
                 li.innerHTML = sDel + names;
                 ul.appendChild(li);
 
 
-                var list = $('#list').getElementsByTagName('span');
-                for(var i = 0; i < list.length; i++){
-                    var listItem = list[i];
+                let list = $('#list').getElementsByTagName('span');
+                for(let i = 0; i < list.length; i++){
+                    let listItem = list[i];
                     listItem.addEventListener('click', deleteThisFunc);
                 }
 
@@ -57,9 +57,9 @@ names.addEventListener("keyup", function(event){
 pickBtn.addEventListener("click", function(){
 
     // find the length of li elements in ul
-    var liLength = ul.getElementsByTagName('li').length;
-    var randomNum = Math.floor(Math.random() * liLength);
-    var pickedLi = ul.getElementsByTagName('li')[randomNum].innerText;
+    let liLength = ul.getElementsByTagName('li').length;
+    let randomNum = Math.floor(Math.random() * liLength);
+    let pickedLi = ul.getElementsByTagName('li')[randomNum].innerText;
 
     // -- DISPLAY PICKED NAME --
     $('#pickedTitle').style.display = "none";
@@ -106,12 +106,29 @@ function deleteThisFunc() {
 
 document.getElementById("add").onclick = function () {
 
-    var names = document.getElementById("names").value;
+    let names = document.getElementById("names").value;
 
 
     if (textBox.value !== '') {
-        li.innerHTML = names;
+        /* When 'add' button is clicked make sure you add the name in the textBox.value to the list */
+        /* Code copied from  names.addEventListener("keyup", function(event) that didn't seem to work */
+        let li = document.createElement('li');
+        li.contentEditable = 'true';
+
+        li.innerHTML = '<span class="deleteThis">x</span>' + /*names*/ textBox.value;
         ul.appendChild(li);
+
+        let list = $('#list').getElementsByTagName('span');
+        for(let i = 0; i < list.length; i++){
+            let listItem = list[i];
+            listItem.addEventListener('click', deleteThisFunc);
+        }
+
+        console.log("names", names);
+        console.log(ul.innerText);
+        /***********************************************************************************************/
+        // li.innerHTML = names;
+        // ul.appendChild(li);
     } else {
         console.log("nothing entered");
 
